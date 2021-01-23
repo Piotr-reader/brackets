4be17b549201a01ca24bb45ea7cc8540fc2e652b
Piotr-reader <piotr_partsianko@inbox.ru>
2021-01-23 12:35:31 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let result = false;
+    for (let i = 0; i < str.length; i++) {
+        if (isOpen(str[i], bracketsConfig)) {
+            stack.push(str[i]);
+        } else if (isClose(str[i], bracketsConfig)) {
+            let chearOpen = stack.pop();
+            let chearClose = str[i];
+            if (isMath(chearOpen, chearClose, bracketsConfig)) {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+        } else {
+            if (stack.length > 0) {
+                if (str[i] === stack[stack.length - 1]) {
+                    stack.pop();
+                    result = true;
+                } else {
+                  stack.push(str[i]);
+                }
+            } else {
+                stack.push(str[i]);
+            }
+        }
+
+    }
+    if (stack.length !== 0) {
+        result = false;
+    }
+    return result;
+};
+
+function isOpen(char, bracketsConfig) {
+    let result = false;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (char === bracketsConfig[i][0] && char !== bracketsConfig[i][1]) {
+            result = true;
+        }
+    }
+    return result;
+}
+
+function isClose(char, bracketsConfig) {
+    let result = false;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (char === bracketsConfig[i][1] && char !== bracketsConfig[i][0]) {
+            result = true;
+        }
+    }
+    return result;
+}
+
+function isMath(chearOpen, chearClose, bracketsConfig) {
+    let result = false;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (chearOpen === bracketsConfig[i][0] && chearClose === bracketsConfig[i][1]) {
+            result = true;
+            break;
+        }
+    }
+    return result;
 }
